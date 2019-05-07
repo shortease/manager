8fcee6e60976c5d8c70b778828202100ba8f24dc
shortease <michael@shortease.com>
2019-05-07 12:24:21 +0300

    crawler

---
+++ components/crawler/assets/crawler.js
@@ -0,0 +1,71 @@
+var Crawler = function() {
+
+	/**
+	*	get channel data from api and run getLinks()
+	**/
+	var crawlChannel = function(channelId) {
+		$.ajax({url : "/?page=crawler&task=api&action=get_crawl_tmplates", data : { channel_id : channelId} })
+			.done(function(response){
+				if (response) {
+					var channel_url 		= response.channel_url;
+					var links_template 		= unescape(response.links_template);
+					var article_template 	= unescape(response.article_template);
+					//po(channel_url, links_template,article_template);
+					getLinks(channelId, channel_url, links_template);
+				}
+			});
+	}
+
+	/**
+	*	crawl to channel address and get article links from it
+	**/
+	var getLinks = function(channelId, channel_url, links_template){
+		var whateverorigin = "http://www.whateverorigin.org/get?url=";
+		var url = 'https://crossorigin.me/' + encodeURIComponent(channel_url);
+		$.ajax({url : url})
+			.done(function(response) {
+				var body_DOM = $(response);
+				var fn = links_template;
+				eval(fn);
+
+				var links = links_template(body_DOM);
+				$.map( links, function( link, i ) {	/// if links are relative vhange it to full links
+					links[i] = (new URL(link,channel_url)).href;
+				});
+				po(links);
+				filterLinks(links);
+			});
+	}
+
+	/**
+	*	send links to api and get from it only new links to crawl
+	**/
+	var filterLinks = function (channelId, links) {
+		$.ajax({url : "/?page=crawler&task=api&action=filter_links", data : { "channel_id" : channelId, "links" : JSON.stringify(links)} })
+			.done(function(response){
+				if (response) {
+					po(response);
+				}
+			});		
+	}
+
+
+	/**
+	*	
+	**/
+
+	/**
+	*	
+	**/
+
+    return {
+        init: function() {
+        },
+        crawlChannel : crawlChannel
+    };
+}();
+
+// Class initialization on page load
+/*jQuery(document).ready(function() {
+    Crawler.init();
+});*/
