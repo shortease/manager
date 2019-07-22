37d8b3a15b72b481b687081784c2bfa0a7252d09
shortease <michael@shortease.com>
2019-07-22 10:20:26 +0300

    add filter by data in general js

--- assets/app/custom/general/general.js
+++ assets/app/custom/general/general.js
@@ -7,3 +7,19 @@ function et (translate_string) {
 function etl (translate_string) {
 	return tl && tl[translate_string] ? tl[translate_string] : translate_string;
 }
+
+(function ($) {
+
+    $.fn.filterByData = function (prop, val) {
+        var $self = this;
+        if (typeof val === 'undefined') {
+            return $self.filter(
+                function () { return typeof $(this).data(prop) !== 'undefined'; }
+            );
+        }
+        return $self.filter(
+            function () { return $(this).data(prop) == val; }
+        );
+    };
+
+})(window.jQuery);
