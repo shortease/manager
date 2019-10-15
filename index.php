0e031e56f63379016fbadf88817c0ee4bf99c63b
shortease <michael@shortease.com>
2019-10-15 10:37:16 +0300

    small changes

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
-
-define( '_SHM_ENABLED_',  1 );
+/*ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+*/define( '_SHM_ENABLED_',  1 );
 /// shortease config
 require_once dirname(__FILE__).'/config.inc';
 /// Enable erate ad server classes 
